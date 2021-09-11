cc5eae14ccb9c0f52c7dd6174ffe624c74479e00
stobiasiewicz <stefan.tobiasiewicz@euvic.pl>
2021-09-11 21:41:51 +0200

    work in progres

---
+++ warsztatSamochodowy/Controllers/ShowProposalController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using warsztatSamochodowy.Forms;
+
+namespace warsztatSamochodowy.Controllers
+{
+    [Authorize(Roles = "manager")]
+    public class ShowProposalController : Controller
+    {
+
+        public List<ShowProposalForm> model { get; set; } = new List<ShowProposalForm>();
+
+        public IActionResult ShowProposal(int proposalId)
+        {
+            this.model.Add(new ShowProposalForm(proposalId));
+            return View(model);
+        }
+    }
+}
---
+++ warsztatSamochodowy/Forms/ShowProposalForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using warsztatSamochodowy.Models;
+using warsztatSamochodowy.Repository;
+
+namespace warsztatSamochodowy.Forms
+{
+    public class ShowProposalForm
+    { 
+        ProposalRepository proposalRepository = new ProposalRepository();
+        ActionRepository actionRepository = new ActionRepository();
+        ClientRepository clientRepository = new ClientRepository();
+        VehicleRepository vehicleRepository = new VehicleRepository();
+
+        public Proposal proposal { get; set; } = new Proposal();
+        public List<Models.Action> actions { get; set; } = new List<Models.Action>();
+        public Vehicle vehicle { get; set; } = new Vehicle();
+        public Client client { get; set; } = new Client();
+
+        public int proposalId { get; set; }
+        public ShowProposalForm(int proposalId)
+        {
+            this.proposalId = proposalId;
+            this.proposal = proposalRepository.GetProposalById(proposalId);
+            this.actions = actionRepository.GetActionsForProposal(proposalId);
+            this.vehicle = vehicleRepository.GetVehicleByRegNum(this.proposal.VehicleId);
+            this.client = clientRepository.getClientById(this.vehicle.ClientId);
+        }
+    }
+}
