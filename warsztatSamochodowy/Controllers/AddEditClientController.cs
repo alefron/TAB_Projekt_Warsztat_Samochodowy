1849b0e2ccc385abf4871b90739a57718df4a295
alefron <ola.front@gmail.com>
2021-09-12 02:08:41 +0200

    edycja/dodawanie klienta

--- warsztatSamochodowy/Controllers/AddEditClientController.cs
+++ warsztatSamochodowy/Controllers/AddEditClientController.cs
@@ -4,15 +4,37 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using warsztatSamochodowy.Forms;
+using warsztatSamochodowy.Repository;
 
 namespace warsztatSamochodowy.Controllers
 {
     [Authorize(Roles = "manager")]
+    
     public class AddEditClientController : Controller
     {
+        public ClientRepository clientRepository = new ClientRepository();
+        public List<FormAddEditClient> model { get; set; } = new List<FormAddEditClient>();
+
+        public AddEditClientController()
+        {
+            FormAddEditClient form = new FormAddEditClient();
+            model.Add(form);
+        }
+
+        [HttpGet("AddEditClient/AddClient")]
         public IActionResult AddClient()
         {
-            return View();
+            return View("AddEditClient", model);
+        }
+
+        [HttpGet("AddEditClient/EditClient")]
+        public IActionResult EditClient(int clientId)
+        {
+            FormAddEditClient form = new FormAddEditClient(clientId);
+            this.model = new List<FormAddEditClient>();
+            this.model.Add(form);
+            return View("AddEditClient", model);
         }
     }
 }
---
+++ warsztatSamochodowy/Forms/FormAddEditClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using warsztatSamochodowy.Models;
+using warsztatSamochodowy.Repository;
+
+namespace warsztatSamochodowy.Forms
+{
+    public class FormAddEditClient
+    {
+        ClientRepository clientRepository = new ClientRepository();
+        AddressRepository addressRepository = new AddressRepository();
+
+
+        public Client client { get; set; } = new Client();
+        public Address address { get; set; } = new Address();
+        public bool is_editable { get; set; }
+
+
+        public FormAddEditClient(int clientId)
+        {
+            this.is_editable = true;
+            this.client = this.clientRepository.getClientById(clientId);
+            this.address = this.addressRepository.GetAddressById(this.client.AddressId);
+        }
+
+        public FormAddEditClient()
+        {
+            this.is_editable = false;
+        }
+    }
+}
